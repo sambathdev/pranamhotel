72c329c109afef0149ffca48fff42c1f7ba1e689
koloka <46620661+koloka@users.noreply.github.com>
2019-01-28 13:58:16 +0700

    update

--- php/index.php
+++ php/index.php
@@ -1,5 +1,17 @@
 <?php
+  define('TITLE','your page title');
   include('../html/template/header.html');
-  include('../html/template/footer.html');
+?>
 
+<div class="indexbody">
+  <!-- write your html here -->
+  <p>start here</p>
+
+
+
+
+</div>
+
+<?php
+include('../html/template/footer.html');
  ?>
