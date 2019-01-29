938dee256772fa50174ed68da612e14a2d8c3626
koloka <46620661+koloka@users.noreply.github.com>
2019-01-29 09:26:33 +0700

    df

---
+++ php/skybar.php
@@ -0,0 +1,18 @@
+<?php
+  define('TITLE','Pranam Hotel');
+  include('../html/template/header.html');
+?>
+
+<div class="indexbody">
+  <!-- write your html here -->
+  <p>start here</p>
+  <p>start here2</p>
+
+
+
+
+</div>
+
+<?php
+include('../html/template/footer.html');
+ ?>
