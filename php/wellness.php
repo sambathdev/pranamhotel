81e71558b523f7831dada617610e57dc68790ac8
koloka <46620661+koloka@users.noreply.github.com>
2019-01-29 20:47:03 +0700

    Update wellness.php

--- php/wellness.php
+++ php/wellness.php
@@ -1,13 +1,11 @@
 <?php
-  define('TITLE','Wellness');
+  define('TITLE','Gym');
   include('../html/template/header.html');
 ?>
 
 <div class="indexbody">
   <!-- write your html here -->
-  <img src="‎⁨../img/wellness/camille-kmile-1120204-unsplash.jpg" alt="dd">
-  <p>meng</p>
-  
+  <img src="../img/wellness/camille-kmile-1120204-unsplash.jpg" alt="">
 
 
 
