5f5ae4f4316721cc0781b19dd9464944ce095094
Dyna19 <47133146+Dyna19@users.noreply.github.com>
2019-01-29 11:57:37 +0700

    update

---
+++ php/history.php
@@ -0,0 +1,18 @@
+<?php
+  define('TITLE','Pool');
+  include('../html/template/header.html');
+?>
+
+<div class="indexbody">
+  <!-- write your html here -->
+  <p>start here</p>
+  <p>start here2</p>
+
+
+
+
+</div>
+
+<?php
+include('../html/template/footer.html');
+ ?>
