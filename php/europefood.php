17520b517340fd5350d38381d85ee7dd5156a822
CSN05 <scho@zamanuniversity.edu.kh>
2019-01-30 01:09:43 +0700

    resturant update

--- php/europefood.php
+++ php/europefood.php
@@ -5,9 +5,60 @@
 
 <div class="indexbody">
   <!-- write your html here -->
-  <p>start here</p>
-  <p>start here2</p>
-
+  <div>
+  <h1 class="food">Europe Food Menu</h1>
+  </div>
+  <div class="menueuropefood">
+  <div class="block">
+		<figure class="z3">
+			<img class="m1" src="../img/europefood/pic3.jpg"/>
+			<figcaption class="f2">Double Salmon with Lemon</figcaption>
+		</figure>
+  </div>
+  <div class="block">
+		<figure class="z3">
+			<img class="m1" src="../img/europefood/pic4.jpg"/>
+			<figcaption class="f2">Stack</figcaption>
+		</figure>
+  </div>
+  <div class="block">
+		<figure class="z3">
+			<img class="m1" src="../img/europefood/pic7.jpg"/>
+			<figcaption class="f2">Pasta with Cherry Tomato</figcaption>
+		</figure>
+  </div>
+  <div class="block">
+		<figure class="z3">
+			<img class="m1" src="../img/europefood/pic10.jpg"/>
+			<figcaption class="f2">Spaghetti</figcaption>
+		</figure>
+  </div>
+  <div class="block">
+		<figure class="z3">
+			<img class="m1" src="../img/europefood/pic11.jpg"/>
+			<figcaption class="f2">Chicken Salad</figcaption>
+		</figure>
+  </div>
+  <div class="block">
+		<figure class="z3">
+			<img class="m1" src="../img/europefood/pic14.jpg"/>
+			<figcaption class="f2">Burger</figcaption>
+		</figure>
+  </div>
+  <div class="block">
+		<figure class="z3">
+			<img class="m1" src="../img/europefood/pic16.jpg"/>
+			<figcaption class="f2">Pizza</figcaption>
+		</figure>
+  </div>
+  <div class="block">
+		<figure class="z3">
+			<img class="m1" src="../img/europefood/pic18.png"/>
+			<figcaption class="f2">Vegetable Egg Pancake</figcaption>
+		</figure>
+  </div>
+  </div>
+  </div>
 
 
 
--- php/restaurant.php
+++ php/restaurant.php
@@ -1,12 +1,66 @@
 <?php
-  define('TITLE','Restaurant');
+  define('TITLE','Resturant');
   include('../html/template/header.html');
 ?>
 
 <div class="indexbody">
   <!-- write your html here -->
-  <p>start here</p>
-  <p>start here2</p>
+  <h1 class="welcome">Welcome to Pranam Resturant</h1>
+  <div class="manpage">
+  <div class="block1">
+	<figure class="z1">
+		<img class="m1" src="../img/resturant/pic1.jpg"/>
+	</figure>
+  </div>
+  <div class="block1">
+	<figure class="z1">
+	<h2>Family Dinner</h2>
+		<p>Pranam Resturant is the best choice for your family dinner. The peaceful
+		environment and special dishes from our chefs are the best for your special people. 
+		We serve the seperate dishes for children also.</p>
+	</figure>
+  </div>
+   
+   <div class="block1">
+	<figure class="z1">
+		<h2 class="hh">Business Dinner Meeting</h2>
+		<p class="hh">Our facilities are specifically designed to accommodate everything from small private 
+		business lunches to world-class conferences in our dedicated Ballroom where there is 
+		room for up to 20 seated guests</p>
+	</figure>
+	</div>
+	<div class="block1">
+	<figure class="z1">
+		<img class="m1" src="../img/resturant/pic2.jpg"/>
+	</figure>
+  </div>
+  <div class="block1">
+	<figure class="z1">
+		<img class="m1" src="../img/resturant/pic3.jpg"/>
+	</figure>
+  </div>
+  <div class="block1">
+	<figure class="z1">
+	<h2>Private Room</h2>
+		<p>We have eight private rooms that can accommodate from six to 60 diners, providing 
+		the perfect environment for getting work done efficiently and securely, while still 
+		enjoying the benefit of Pranam Hotel and Resturant.</p>
+	</figure>
+  </div>
+  <div class="block1">
+	<figure class="z1">
+		<h2 class="hh">Small Party</h2>
+		<p class="hh">Pranam Resturant serves the best for your best moments with your family and your friends.
+		Pranam is the best for organizing the small party or birthday party with your closet ones.</p>
+	</figure>
+	</div>
+	<div class="block1">
+	<figure class="z1">
+		<img class="m1" src="../img/resturant/pic4.jpg"/>
+	</figure>
+  </div>
+  
+  </div>
 
 
 
