85da58dba8ca669148a4bbeeb8f1d206cf1ecf8f
koloka <46620661+koloka@users.noreply.github.com>
2019-01-29 09:33:12 +0700

    img

--- php/index.php
+++ php/index.php
@@ -5,8 +5,19 @@
 
 <div class="indexbody">
   <!-- write your html here -->
-  <p>start here</p>
-  <p>start here2</p>
+  <div class="bbco">
+    <div class="center">
+      <div class="">
+        <h1>Breakfast</h1>
+        <h4>Operation Hours: 06:00 am – 10:30 pm (daily)</h4>
+        <p>An upscale casual dine – in and to-go bakery offering fresh baked goods, breakfast sandwiches, omelets, coffee, Teas, fresh fruit smoothies, specialty sandwiches, salads, soups, bowls, cheesecake and more. In 2018, BBCO expand its alfresco area so call “BBCO Terrace” in aiming to provide a new dining experience to guests.</p>
+      </div>
+      <div class="">
+        <img src="" alt="">
+      </div>
+
+    </div>
+  </div>
 
 
 
