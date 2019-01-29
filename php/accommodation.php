662d93abd62ba0677b26974ff8e5f8c5a902c056
koloka <46620661+koloka@users.noreply.github.com>
2019-01-29 17:54:16 +0700

    update link

--- php/accommodation.php
+++ php/accommodation.php
@@ -16,28 +16,28 @@
 
 
   <div class="gallery">
-  <a target="_blank" href="deluxeroom.jpg">
+  <a href="deluxeroom.php">
     <img src="../img/room/deluxeroom.jpg" alt="Cinque Terre" width="1200" height="800">
   </a>
   <div class="desc">Deluxe Room</div>
 </div>
 
 <div class="gallery">
-<a target="_blank" href="deluxeroom.jpg">
+<a href="premierroom.php">
   <img src="../img/room/premierroom.jpg" alt="Cinque Terre" width="1200" height="800">
 </a>
 <div class="desc">Premier Room</div>
 </div>
 
 <div class="gallery">
-<a target="_blank" href="executiveroom.jpg">
+<a href="executiveroom.php">
   <img src="../img/room/executiveroom.jpg" alt="Cinque Terre" width="1200" height="800">
 </a>
 <div class="desc">Executive Room</div>
 </div>
 
 <div class="gallery">
-<a target="_blank" href="deluxeroom.jpg">
+<a href="meetingandevent.php">
   <img src="../img/room/eventroom.jpg" alt="Cinque Terre" width="1200" height="800">
 </a>
 <div class="desc">Event Room</div>
