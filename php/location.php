354aeeaa04804c1a1ed78dd02fdad68a01af30fe
koloka <46620661+koloka@users.noreply.github.com>
2019-01-30 09:34:35 +0700

    Update location.php

--- php/location.php
+++ php/location.php
@@ -5,7 +5,7 @@
 
 <div class="indexbody">
   <!-- write your html here -->
-  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3908.699349557424!2d104.91666691431868!3d11.573397391783718!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31095143a986fc07%3A0xc4976c41ebdc86bf!2z4Z6i4Z624Z6C4Z624Z6a4Z6c4Z6M4Z-S4Z6N4Z6T4Z-I!5e0!3m2!1sen!2skh!4v1548781517477" frameborder="0" width="100%" height="100%" style="border:0" allowfullscreen></iframe>
+  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3908.699349557424!2d104.91666691431868!3d11.573397391783718!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31095143a986fc07%3A0xc4976c41ebdc86bf!2z4Z6i4Z624Z6C4Z624Z6a4Z6c4Z6M4Z-S4Z6N4Z6T4Z-I!5e0!3m2!1sen!2skh!4v1548781517477" frameborder="0" width="100%" height="800px" style="border:0" allowfullscreen></iframe>
 
 
 
