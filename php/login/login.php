eeead0555776f73cba4cf2c4925ce42ad3df26d5
koloka <46620661+koloka@users.noreply.github.com>
2019-02-02 23:43:57 +0700

    e

--- php/login/login.php
+++ php/login/login.php
@@ -3,15 +3,70 @@
   include('../../html/template/header.html');
 ?>
 
-<div class="indexbody">
-  <!-- write your html here -->
-  <p>start here</p>
-  <p>start here2</p>
 
 
 
 
-</div>
+
+<?php
+
+if (isset($_POST['login_user'])) {
+
+  if (empty($username)) {
+    array_push($errors, "Username is required");
+  }
+  if (empty($password)) {
+    array_push($errors, "Password is required");
+  }
+
+  if (count($errors) == 0) {
+    $password = $password;
+    $query = "SELECT * FROM users WHERE name='$username' AND password='$password'";
+    $results = mysqli_query($db, $query);
+
+    if (mysqli_num_rows($results) == 1) {
+      $_SESSION['username'] = $username;
+      $_SESSION['success'] = "You are now logged in";
+      header('location: index.php');
+    }else {
+      array_push($errors, "Wrong username/password combination");
+    }
+  }
+}
+
+ ?>
+
+	<div class="header">
+		<h2>Login</h2>
+	</div>
+
+	<form method="post" action="login.php">
+
+		<?php include('errors.php'); ?>
+
+		<div class="input-group">
+			<label>Username</label>
+			<input type="text" name="username" >
+		</div>
+		<div class="input-group">
+			<label>Password</label>
+			<input type="password" name="password">
+		</div>
+		<div class="input-group">
+			<button type="submit" class="btn" name="login_user">Login</button>
+		</div>
+		<p>
+			Not yet a member? <a href="register.php">Sign up</a>
+		</p>
+	</form>
+
+
+
+
+
+
+
+
 
 <?php
 include('../../html/template/footer.html');
