395574d48008fd57cd6490c4b89021b47f2d5eca
koloka <46620661+koloka@users.noreply.github.com>
2019-02-05 18:06:07 +0700

    s

--- php/login/login.php
+++ php/login/login.php
@@ -5,7 +5,7 @@ define('TITLE','Login');
 include('../../html/template/header-login.html');
 // Print some introductory text:
 print '<h2 class="logintitle1">Login Form</h2>
-	<p class="logintitle1">Users who are logged in can take advantage of certain features like this, that, and the other thing.</p>';
+	<p class="logintitle2">Users who are logged in can take advantage of certain features like this, that, and the other thing.</p>';
 // Check if the form has been submitted:
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
@@ -25,20 +25,35 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 		} else { // Incorrect!
 
 			print '<p class="text--error">The submitted email address and password do not match those on file!<br>Go back and try again.</p>';
-			print '<a href="login.php"><p class = "loginagain">login again! </p></a>';
+			print '<a href="login.php" class = "loginagain"><p>login again! </p></a>';
 		}
 	} else { // Forgot a field.
 		print '<p class="text--error">Please make sure you enter both an email address and a password!<br>Go back and try again.</p>';
-		print '<a href="login.php"><p class = "loginagain">login again! </p></a>';
+		print '<a href="login.php" class = "loginagain"><p>login again! </p></a>';
 	}
 
 } else { // Display the form.
 
-	print '<form action="login.php" method="post" class="form--inline">
-	<p><label for="email">Email Address:</label><input type="email" name="email" size="20"></p>
-	<p><label for="password">Password:</label><input type="password" name="password" size="20"></p>
-	<p><input type="submit" name="submit" value="Log In!" class="button--pill"></p>
-	</form>';
+	print '<form class="loginform" action="login.php" method="post" class="form--inline">
+	<table>
+		<tr class="rowheight">
+			<td><label for="email">Email Address:</label></td>
+			<td><input class="boxsize" type="email" name="email" size="30"></td>
+		</tr>
+		<tr class="rowheight">
+			<td><label for="password">Password:</label></td>
+			<td><input class="boxsize" type="password" name="password" size="30"></td>
+		</tr>
+		<tr>
+			<td><p><input type="submit" name="submit" value="Log In!" class="button--pill"></p></td>
+			<td><a href="../../index.php" class="discover"><p>Discover without Login</p></a></td>
+		</tr>
+	</table>
+
+
+	</form>
+
+	';
 }
 include('../../html/template/footer-login.html');
 ?>
--- php/login/logout.php
+++ php/login/logout.php
@@ -16,8 +16,8 @@ include('../../html/template/header.html');
 
 ?>
 <div class="indexbody">
-<p class="logintitle">You are now logged out.</p>
-<p class="logintitle">Thank you for using this site. We hope that you liked it.<br></p>
+<p class="logouttitle">You are now logged out.</p>
+<p class="logouttitle">Thank you for using this site. We hope that you liked it.<br></p>
 </div>
 
 <?php include('../../html/template/footer.html'); // Need the footer. ?>
