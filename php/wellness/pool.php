15e01b6a9c656804a594a2378b038786a0d0ded2
MengBoi <41507257+MengBoi@users.noreply.github.com>
2019-02-07 14:18:42 +0700

    .

--- php/wellness/pool.php
+++ php/wellness/pool.php
@@ -5,8 +5,11 @@
 ?>
 
 <div class="indexbody">
-  <h1>Welcome to Pranam Pool</h1>
+  <h1 class="pool-welcome-header">Welcome to Pranam Pool</h1>
   <div class="pool-blog-container">
+    <img class="pool-img-in-blog" src="../../img/wellness/ZOLK2SVVJRFJRGVFBFBSOOLGS4.jpg" alt="">
+    <p class="pool-preview-in-blog">Just like anything else, swimming pools come in all different types, designs, sizes, shapes and are used for various purposes. Budget, lot size, local codes, and availability of materials will all factor into what type of pool you will eventually decide to build or install on your property.</p>
+    <p class="readMoreButton-in-blog">Read more</p>
   </div>
 
 
