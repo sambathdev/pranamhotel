7bd7507e919189d6e83594c2acb90c70c5fe78d6
koloka <46620661+koloka@users.noreply.github.com>
2019-02-10 19:42:16 +0700

    s

---
+++ php/sitemap/sitemap.php
@@ -0,0 +1,74 @@
+<?php
+  session_start();
+  define('TITLE', 'News & Promotions');
+  include('../../html/template/header.html');
+?>
+<!-- include header -->
+
+<div class="body">
+  <h1 class="pagetitle">News & Promotions</h1>
+  <div class="blockcontain">
+    <div class="center">
+      <div class="makegrid">
+        <div class="block">
+          <img src="../../img/block1.jpg" alt="">
+          <div class="describtion">
+            <h1>BOND WITH DAD AT JW MARRIOTT HOTEL BANGKOK</h1>
+            <p>Give your dad the meal he deserves for Father’s Day! Treat the old man to a hearty and happy meal at one of his favourite restaurants on Father’s Day. Whether your dad loves a heavy Grand Brunch Buffet or a Dim Sum-filled feast, JW Marriott Hotel Bangkok with great Father’s […]</p>
+          </div>
+          <a href="#">More</a>
+        </div>
+        <div class="block">
+          <img src="../../img/room/premierroom2.jpg" alt="">
+          <div class="describtion">
+            <h1>BOND WITH DAD AT JW MARRIOTT HOTEL BANGKOK</h1>
+            <p>Give your dad the meal he deserves for Father’s Day! Treat the old man to a hearty and happy meal at one of his favourite restaurants on Father’s Day. Whether your dad loves a heavy Grand Brunch Buffet or a Dim Sum-filled feast, JW Marriott Hotel Bangkok with great Father’s […]</p>
+          </div>
+          <a href="#">More</a>
+        </div>
+        <div class="block">
+          <img src="../../img/wellness/camille-kmile-1120204-unsplash.jpg" alt="">
+          <div class="describtion">
+            <h1>BOND WITH DAD AT JW MARRIOTT HOTEL BANGKOK</h1>
+            <p>Give your dad the meal he deserves for Father’s Day! Treat the old man to a hearty and happy meal at one of his favourite restaurants on Father’s Day. Whether your dad loves a heavy Grand Brunch Buffet or a Dim Sum-filled feast, JW Marriott Hotel Bangkok with great Father’s […]</p>
+          </div>
+          <a href="#">More</a>
+        </div>
+        <div class="block">
+          <img src="../../img/wellness/https___blueprint-api-production.s3.amazonaws.com_uploads_card_image_171914_EVEN.jpg" alt="">
+          <div class="describtion">
+            <h1>BOND WITH DAD AT JW MARRIOTT HOTEL BANGKOK</h1>
+            <p>Give your dad the meal he deserves for Father’s Day! Treat the old man to a hearty and happy meal at one of his favourite restaurants on Father’s Day. Whether your dad loves a heavy Grand Brunch Buffet or a Dim Sum-filled feast, JW Marriott Hotel Bangkok with great Father’s […]</p>
+          </div>
+          <a href="#">More</a>
+        </div>
+        <div class="block">
+          <img src="../../img/wellness/ZOLK2SVVJRFJRGVFBFBSOOLGS4.jpg" alt="">
+          <div class="describtion">
+            <h1>BOND WITH DAD AT JW MARRIOTT HOTEL BANGKOK</h1>
+            <p>Give your dad the meal he deserves for Father’s Day! Treat the old man to a hearty and happy meal at one of his favourite restaurants on Father’s Day. Whether your dad loves a heavy Grand Brunch Buffet or a Dim Sum-filled feast, JW Marriott Hotel Bangkok with great Father’s […]</p>
+          </div>
+          <a href="#">More</a>
+        </div>
+        <div class="block">
+          <img src="../../img/meeting/7272_sm_05_p_2048x1536.jpg" alt="">
+          <div class="describtion">
+            <h1>BOND WITH DAD AT JW MARRIOTT HOTEL BANGKOK</h1>
+            <p>Give your dad the meal he deserves for Father’s Day! Treat the old man to a hearty and happy meal at one of his favourite restaurants on Father’s Day. Whether your dad loves a heavy Grand Brunch Buffet or a Dim Sum-filled feast, JW Marriott Hotel Bangkok with great Father’s […]</p>
+          </div>
+          <a href="#">More</a>
+        </div>
+      </div>
+    </div> <!-- end center =============== -->
+  </div> <!-- end bodycontain -->
+
+
+</div> <!-- end body div =============== -->
+
+
+
+
+<!-- include footer -->
+<?php
+  include('../../html/template/footer.html');
+?>
