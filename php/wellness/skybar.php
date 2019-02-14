3e1f3333e7a62f09be7c71b8997bca075c2d5b8d
MengBoi <41507257+MengBoi@users.noreply.github.com>
2019-02-14 15:29:26 +0700

    .

--- php/wellness/skybar.php
+++ php/wellness/skybar.php
@@ -6,9 +6,34 @@
 
 <div class="indexbody">
   <!-- write your html here -->
-  <p>start here</p>
-  <p>start here2</p>
-
+  <h1 class="wellness-skybar-welcome">Welcome to Pranam Skybar</h1>
+  <div class="wellness-skybar-blog-container">
+    <img class="wellness-skybar-img-in-blog" src="../../img/wellness/sky-bar-3.jpg" alt="">
+    <h2 class="wellness-skybar-h2-in-blog">Romantic Skybar in the town</h2>
+    <p class="wellness-skybar-preview-in-blog">When the weather is warm, rooftop bars and restaurants become playgrounds for city dwellers. Nights are hot, sunsets are radiant and drinks always taste better when sipped in the sky. But to make our list of the best rooftop bars in the world, these 13 hot spots had to offer more than dreamy views. Plan your experience around sunset to watch the Tower’s lights begin to glitter as the sun sinks below the horizon.</p>
+    <p class="wellness-skybar-reservation-in-blog" >RESERVATIONS:</p>
+    <p class="wellness-skybar-reservation-p-in-blog">At least, one day before</p>
+    <p class="wellness-skybar-reservation-p-in-blog">Tlf: +34 933 014 484</p>
+    <p class="wellness-skybar-reservation-p-in-blog">e-mail: hotel@hotelcurious.com</p>
+  </div>
+  <div class="wellness-skybar-blog-container">
+    <img class="wellness-skybar-img-in-blog" src="../../img/wellness/m-odbarcelona-03-odskybar-02561.jpg" alt="">
+    <h2 class="wellness-skybar-h2-in-blog">VIP lounge</h2>
+    <p class="wellness-skybar-preview-in-blog">Recommended by the Concierge team at Four Seasons Hotel George V, Paris, this rooftop restaurant occupies the top floor of the Jean Nouvel–designed Musée du quai Branly in the 7th district, and is an easy jaunt from the Hotel. Take a tour of the museum’s exhibitions, or skip the culture and proceed directly to the celebrated menu of French classics. A restaurant for travellers and locals alike, Les Ombres boasts unmatched views of the Eiffel Tower from the terrace.</p>
+    <p class="wellness-skybar-reservation-in-blog" >RESERVATIONS:</p>
+    <p class="wellness-skybar-reservation-p-in-blog">At least, one day before</p>
+    <p class="wellness-skybar-reservation-p-in-blog">Tlf: +34 933 014 484</p>
+    <p class="wellness-skybar-reservation-p-in-blog">e-mail: hotel@hotelcurious.com</p>
+  </div>
+  <div class="wellness-skybar-blog-container">
+    <img class="wellness-skybar-img-in-blog" src="../../img/wellness/sky-bar-3.jpg" alt="">
+  </div>
+  <div class="wellness-skybar-blog-container">
+    <img class="wellness-skybar-img-in-blog" src="../../img/wellness/sky-bar-3.jpg" alt="">
+  </div>
+  <div class="wellness-skybar-blog-container">
+    <img class="wellness-skybar-img-in-blog" src="../../img/wellness/sky-bar-3.jpg" alt="">
+  </div>
 
 
 
