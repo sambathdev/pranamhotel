d5ed0add6ef41376693c87d8ebec4ae3d703c1e5
koloka <46620661+koloka@users.noreply.github.com>
2019-02-20 13:25:56 +0700

    sd

--- php/admin/view_comment.php
+++ php/admin/view_comment.php
@@ -10,18 +10,25 @@
   <div class="indexbody">
 
     <?php
+    $name = $_SESSION['username'];
     $query = 'SELECT * FROM comments ORDER BY comment_date DESC';
+    $queryprofile = "SELECT * FROM users where name = '$name'";
+    if($a = mysqli_query($dbc, $queryprofile)){
+      while ($rowprofile = mysqli_fetch_array($a)) {
+        $pathprofile = $rowprofile['pathpro'];
+      }
+    }
     if ($r = mysqli_query($dbc, $query)) {
       while ($row = mysqli_fetch_array($r)) {
         print
         '<div class="center">
           <div class="commentblock">'.
+            '<img href="'. $rowprofile['profile'] .'">'.
             '<P class="commentname"><b>'.$row['first_name'] ." ". $row['last_name'].'</b>'.' From '.$row['country'].'</P>'.
             '<P>'.$row['comment'].'</P>'.
             '<a href="delete_entry.php?id='.$row['id'].'">Delete</a>'
           .'</div>
         </div>';
-
       }
     }
      ?>
